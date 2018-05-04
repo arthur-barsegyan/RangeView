8bf06cc2ce523d04b92117afee0385684ee5b413
Arthur Barsegyan <a.barsegyan96@gmail.com>
2018-05-04 18:53:41 +0700

    Implement other methods

--- views.hpp
+++ views.hpp
@@ -1,8 +1,12 @@
 #import <vector>
 #import <functional>
+#import <algorithm>
 #import <iostream>
 
 namespace view {
+
+template<typename T>
+class RangeView;
 
 template<typename Func>
 class TerminationOp {
@@ -10,8 +14,8 @@ public:
 	TerminationOp(Func _f) : f(_f) {}
 
 	template<typename T>
-	auto apply(std::vector<T> &v) {
-		return f(v);
+	auto apply(std::vector<T> &v, RangeView<T> &rv) {
+		return f(v, rv);
 	}
 private:
 	Func &f;
@@ -20,16 +24,20 @@ private:
 template<typename T>
 class RangeView {
 public:
-	typedef std::function< std::vector<T>(std::vector<T>&) > Action;
+	typedef std::function< std::vector<T>(std::vector<T>&, RangeView<T>&) > Action;
 	typedef std::vector<Action> Actions;
 
 	RangeView(Action func) {
 		actions.push_back(func);
 	}
 
-	// RangeView operator|(RangeView &another) {
+	void setCount(size_t _count) {
+		count = _count;
+	}
 
-	// }
+	size_t getCount() {
+		return count;
+	}
 
 	template<class Y, class Z>
 	friend RangeView<Y> operator|(std::vector<Y> &v, Z func);
@@ -57,7 +65,8 @@ private:
 		actions.push_back(action);
 	}
 
-	int currentAction = 0;
+	size_t count = 0;
+	
 	std::vector<T> *v;
 	Actions actions;	
 };
@@ -65,7 +74,7 @@ private:
 template<typename T, typename F>
 RangeView<T> operator|(std::vector<T> &vec, F func) {
 	std::cout << "Create RangeView stream" << std::endl;
-	RangeView<T> rv = RangeView<T>(std::function< std::vector<T>(std::vector<T>&) > (func));
+	RangeView<T> rv = RangeView<T>(std::function< std::vector<T>(std::vector<T>&, RangeView<T>&) >(func));
 	rv.setCollection(&vec);
 
 	return rv;
@@ -73,7 +82,7 @@ RangeView<T> operator|(std::vector<T> &vec, F func) {
 
 template<typename T, typename F>
 RangeView<T> operator|(RangeView<T> rv, F func) {
-	rv.addAction(std::function< std::vector<T>(std::vector<T>&) >(func));
+	rv.addAction(std::function< std::vector<T>(std::vector<T>&, RangeView<T>&) >(func));
 	return rv;
 }
 
@@ -81,15 +90,50 @@ template<typename T, typename F>
 auto operator|(RangeView<T> rv, TerminationOp<F> func) {
 	std::vector<T> &result = rv.getCollection(); 
 	for (auto &act : rv.getActions()) {
-		result = act(result);
+		act(result, rv);
 	}
 
-	return func.apply(result);
+	return func.apply(result, rv);
 }
+
+auto ints(int n) {
+	auto int_generator_func = [n](auto &v, auto &rv) {
+		for (size_t i = n; i < rv.getCount(); i++) {
+			v.push_back(i);
+		}
+
+		return v;
+	};
+
+	return int_generator_func;
+}
+
+auto take(int n) {
+	auto take_func = [n](auto &v, auto &rv) {
+		rv.setCount(n);
+		return v;
+	};
+
+	return take_func;
+}
+
+auto reverse() {
+	auto reverse_func = [](auto &v, auto &rv) {
+		std::reverse(v.begin(), v.end());
+		return v;
+	};
+
+	return reverse_func;
+}
+
+// template <class TView>
+// auto accumulate(TView view) {
+
+// }
 
 template<typename F>
 auto remove_if(F &&pred) {
-	auto remove_if_func = [pred](auto &v) {
+	auto remove_if_func = [pred](auto &v, auto &rv) {
 		for (size_t i = 0; i < v.size();) {
 			if (pred(v[i])) {
 				v.erase(v.begin() + i);
@@ -106,7 +150,7 @@ auto remove_if(F &&pred) {
 
 template<typename F>
 auto transform(F &&pred) {
-	auto transform_func = [pred](auto &v) {
+	auto transform_func = [pred](auto &v, auto &rv) {
 		using type = std::decay_t<decltype(pred(v[0]))>;
 
 		std::vector<type> result;
